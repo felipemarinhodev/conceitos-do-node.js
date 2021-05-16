ff405163a98f37ad6b2c78ec093f220867c84406
Felipe Marinho <felipmarinho@gmail.com>
2021-05-16 18:35:20 -0300

    saving user and doing check

--- src/index.js
+++ src/index.js
@@ -1,21 +1,39 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.body;
+  const user = users.filter(user => user.username === username);
+  if (!user) {
+    return response.status(400).json({ error: 'Messangem de erro' })
+  }
+  request.user = user
+  return next();
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name , username } = request.body;
+  const userAlreadySaved = users.some(user => user.username === username)
+  if (userAlreadySaved) {
+    return response.status(400).json({ error: 'Messangem de erro' })
+  }
+  const newUser = {
+    id: uuidv4(),
+    name,
+    username,
+    todos: []
+  }
+  users.push(newUser);
+  return response.status(201).json(newUser);
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
